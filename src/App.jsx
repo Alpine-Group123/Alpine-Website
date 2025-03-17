a57116281c524f2681bbbbf1c202fe79362027c9
Kafolabi <koladeafolabi2020@gmail.com>
2025-03-17 14:30:10 +0100

    Refactor: Update App.jsx to improve component structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import React from "react";
-// import Home from "./Components/Home";
-import About from "./Components/About";
-import Navigation from "./Components/Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./App.css";
+import Gallery from "./components/Gallery";
+import MapComponent from "./components/MapComponent";
+import Navigation from "./components/Navigation";
+import About from "./components/About";
+
 function App() {
+  const images = Object.values(
+    import.meta.glob("./assets/mountaingallery/*.jpg", { eager: true })
+  ).map((module) => module.default);
+
   return (
-    <div>
+    <div className="container">
       <Navigation />
       <About />
+      <MapComponent />
+      <h1
+        className="display text-center text-bold text-main"
+        style={{ marginTop: "9rem" }}
+      >
+        OUR CLIMBS
+      </h1>
+      <p className="text-center">
+        We have climbed the highest mountains just to get a better view of the
+        earth below.
+      </p>
+      <Gallery images={images} />
     </div>
   );
 }
