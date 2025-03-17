717501de28fe874cd3b3efab4afc46f00e30dfd8
Kafolabi <koladeafolabi2020@gmail.com>
2025-03-17 14:23:47 +0100

    Refactor: Update App component logic for improved readability

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import React from "react";
 import About from "./Components/About";
 import Navigation from "./Components/Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 function App() {
   return (
     <div>
       <Navigation />
       <About />
     </div>
-
   );
 }
 
